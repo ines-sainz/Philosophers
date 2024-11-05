a5001f40af928a3ab997dda8713cd19126fe5829
Ines Sainz Ramon <isainz-r@c1r2s4.42madrid.com>
2024-11-05 16:59:55 +0100

    create as many threads as philos i have. Also wait for them. coooool :)

--- threads.c
+++ threads.c
@@ -12,34 +12,29 @@
 
 #include "philosophers.h"
 
-pthread_mutex_t	mutex; //inicializarlo de manera global
-
 void	*do_smth(void *arg)
 {
 	t_simulation *sim;
 
 	sim = (t_simulation *)arg;
-	printf("hola hilos %li\n", sim->n_philos);
 
 	//PARA BLOQUEAR Y DESBLOQUEAR UNA SECCIÓN
 	//pasamos la dirrección de mmemmoria del mutex
 	int i = 0;
 	while (i < 10000)
 	{
-		pthread_mutex_lock(&mutex);
-		sim->n_philos += 1;
-		pthread_mutex_unlock(&mutex);
+		pthread_mutex_lock(&sim->mutex);
+		sim->test += 1;
+		pthread_mutex_unlock(&sim->mutex);
 		i++;
 	}
-	printf("des                    pues %li\n", sim->n_philos);
+	printf("hola hilo %i\n", sim->test);
 
 	return (NULL);
 }
 
 int	create_threads(t_simulation sim)
 {
-	pthread_t		thread;
-	pthread_t		thread2;
 	int				created;
 
 	(void)sim;
@@ -47,7 +42,7 @@ int	create_threads(t_simulation sim)
 	//INICIALIZAMOS EL MUTEX
 	//dirección de memoria del mutex
 	//argumentos
-	pthread_mutex_init(&mutex, NULL);
+	pthread_mutex_init(&sim.mutex, NULL);
 
 	//CREAMOS EL HILO
 	//direccion de memoria del hilo
@@ -55,24 +50,30 @@ int	create_threads(t_simulation sim)
 	//funcion con las tareas del hilo
 	//parámetros de la función
 	printf("n_philos= %li\n", sim.n_philos);
-	created = pthread_create(&thread, NULL, do_smth, (void *)(&sim));
-	printf("n_philos200000000000000000000= %li\n", sim.n_philos);
-	created = pthread_create(&thread2, NULL, do_smth, (void *)(&sim));
-	printf("n_philos200000000000000000000= %li\n", sim.n_philos);
-
-	if (created != 0)
-		return (1);
+	sim.p_iterator = sim.first_philo;
+	while (sim.p_iterator)
+	{
+		created = pthread_create(&(sim.p_iterator->thread_id), NULL, do_smth, (void *)(&sim));
+		if (created != 0)
+			return (1);
+		printf("created philo %i\n", sim.p_iterator->n_philo);
+		sim.p_iterator = sim.p_iterator->next;
+	}
 
 	//CONDICIONES DE CARRERA
 	//EVITAR QUE EL HILO TERMINE ANTES QUE OTRO O QUE NO HAYA TERMINADO AL FINAL
 	//nombre del hilo a esperar
 	//valor que esperamos que devuelva la funcion que ejecuta el hilo al terminar
 	//(hay que poner la variable donde se va a ejecutar eel hilo al terminar)
-	pthread_join(thread, NULL);
-	pthread_join(thread2, NULL);
+	sim.p_iterator = sim.first_philo;
+	while (sim.p_iterator)
+	{
+		pthread_join(sim.p_iterator->thread_id, NULL);
+		sim.p_iterator = sim.p_iterator->next;
+	}
 
 	//DESTRUIMOS EL MUTEX
 	//direccion de memoria del mutex
-	pthread_mutex_destroy(&mutex);
+	pthread_mutex_destroy(&sim.mutex);
 	return (0);
 }
